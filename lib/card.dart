f2326dc1e46aae0c4f126f6ba415026d4f47a13d
Olivier Boisard <olivier.boisard@gsensing.eu>
2020-01-29 17:36:55 +0100

    Designed cards.

--- lib/card.dart
+++ lib/card.dart
@@ -3,17 +3,78 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FaceDownCard extends StatelessWidget {
+abstract class AbstractCard extends StatelessWidget {
+  static final height = (0.04 * window.physicalSize.height);
+  static final width = height / 2;
+  static final borderRadius = height / 20;
+}
+
+class FaceDownCard extends AbstractCard {
   @override
   Widget build(BuildContext context) {
-    final height = 0.04 * window.physicalSize.height;
     return Container(
-      height: height,
-      width: height / 2,
+      height: AbstractCard.height,
+      width: AbstractCard.width,
       decoration: BoxDecoration(
         color: Colors.deepPurple,
         border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(height / 20),
+        borderRadius: BorderRadius.circular(AbstractCard.borderRadius),
+      ),
+    );
+  }
+}
+
+class FaceUpCard extends StatelessWidget {
+  final suit = "♠";
+  final value = "R";
+  static final cardValueToEmoji = {
+    "R": "♔",
+    "D": "♕",
+    "C": "♘",
+    "V": "♗",
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    var centerValueText = (cardValueToEmoji.containsKey(value)
+        ? cardValueToEmoji[value]
+        : value);
+    final smallTextWidget = Text(
+      "$value\n$suit",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: window.physicalSize.width / 150,
+      ),
+    );
+    final row = Row(
+      children: <Widget>[
+        smallTextWidget,
+        Expanded(
+          child: Container(),
+        ),
+        smallTextWidget,
+      ],
+    );
+
+    return Container(
+      height: AbstractCard.height,
+      width: AbstractCard.width,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(AbstractCard.borderRadius),
+      ),
+      child: Column(
+        children: <Widget>[
+          row,
+          Expanded(
+            child: Align(
+              child: Text("$centerValueText$suit", textAlign: TextAlign.center),
+              alignment: Alignment.center,
+            ),
+          ),
+          row,
+        ],
       ),
     );
   }
