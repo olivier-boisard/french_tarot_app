0552e44838032d9f64634bbb5aba102e50ac1c53
Olivier Boisard <olivier.boisard@gmail.com>
2020-02-10 14:44:48 +0100

    Added unit test for when some cards are not allowed.

--- test/engine/turn_test.dart
+++ test/engine/turn_test.dart
@@ -23,4 +23,15 @@ void main() {
     final allowedCards = turn.extractAllowedCards(cards);
     expect(allowedCards.length, equals(cards.length));
   });
+  test("All spades is asked, some cards in hand are allowed", () {
+    final turn = Turn();
+    turn.addPlayedCard(Card.coloredCard(Suit.spades, 1));
+    final Iterable<Card> cards = [
+      Card.coloredCard(Suit.spades, 2),
+      Card.coloredCard(Suit.heart, 3),
+      Card.coloredCard(Suit.spades, 4)
+    ];
+    final allowedCards = turn.extractAllowedCards(cards);
+    expect(allowedCards.length, equals(2));
+  });
 }
