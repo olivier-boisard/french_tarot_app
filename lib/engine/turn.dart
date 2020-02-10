03d932bcbe0d824fec0827025829077cab0fbf79
Olivier Boisard <olivier.boisard@gmail.com>
2020-02-10 14:32:44 +0100

    Implemented Turn.addPlayedCard().

--- lib/engine/turn.dart
+++ lib/engine/turn.dart
@@ -1,1 +1,11 @@
-class Turn {}
+import 'package:french_tarot/engine/card.dart';
+
+class Turn {
+  final List<Card> playedCards;
+
+  Turn() : playedCards = List<Card>();
+
+  void addPlayedCard(Card card) {
+    playedCards.add(card);
+  }
+}
--- test/engine/turn_test.dart
+++ test/engine/turn_test.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:french_tarot/engine/card.dart';
 import 'package:french_tarot/engine/turn.dart';
 
 void main() {
   test("Construct Turn object", () {
     expect(() => Turn(), returnsNormally);
+  });
+  test("Add card", () {
+    final turn = Turn();
+    final playedCard = Card.trump(2);
+    turn.addPlayedCard(playedCard);
+    expect(turn.playedCards.first, equals(playedCard));
   });
 }
