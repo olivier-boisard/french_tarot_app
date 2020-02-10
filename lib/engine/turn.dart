eb6f2c300e90c3e55d86d6055bad1c0dea26ea9a
Olivier Boisard <olivier.boisard@gmail.com>
2020-02-10 15:27:06 +0100

    Refactored Turn.extractAllowedCards()

--- lib/engine/turn.dart
+++ lib/engine/turn.dart
@@ -5,32 +5,59 @@ class Turn {
 
   Turn() : playedCards = List<Card>();
 
+  bool get onlyExcuseWasPlayed {
+    return playedCards.first == Card.excuse() && playedCards.length == 1;
+  }
+
+  Suit get askedSuit {
+    final firstPlayedCard = playedCards.first;
+    final asked = firstPlayedCard != Card.excuse()
+        ? firstPlayedCard.suit
+        : playedCards[1].suit;
+    return asked;
+  }
+
   void addPlayedCard(Card card) {
     playedCards.add(card);
   }
 
   List<Card> extractAllowedCards(List<Card> hand) {
     if (playedCards.isEmpty) {
-      return List<Card>.from(hand);
-    }
-    var firstCard = playedCards.first;
-    if (firstCard == Card.excuse() && playedCards.length == 1) {
-      return List<Card>.from(hand);
+      return _copyCardList(hand);
     }
 
-    final asked = firstCard != Card.excuse()
-        ? firstCard.suit
-        : playedCards[1].suit;
-    var validCards = hand.where((card) => card.suit == asked).toList();
+    if (onlyExcuseWasPlayed) {
+      return _copyCardList(hand);
+    }
+
+    List<Card> validCards = _extractCardsMatchingAskedSuit(hand);
     if (validCards.isEmpty) {
-      validCards = hand.where((card) => card.suit == Suit.trump).toList();
+      validCards = _extractTrumps(hand);
       if (validCards.isEmpty) {
-        validCards = List<Card>.from(hand);
+        return _copyCardList(hand);
       }
     }
-    if (hand.any((card) => card == Card.excuse())) {
+    if (_containsExcuse(hand)) {
       validCards.add(Card.excuse());
     }
     return validCards;
   }
+
+  List<Card> _copyCardList(List<Card> hand) {
+    return List<Card>.from(hand);
+  }
+
+  List<Card> _extractCardsMatchingAskedSuit(List<Card> hand) {
+    var validCards = hand.where((card) => card.suit == askedSuit).toList();
+    return validCards;
+  }
+
+  List<Card> _extractTrumps(List<Card> hand) {
+    return hand.where((card) => card.suit == Suit.trump).toList();
+  }
+
+  bool _containsExcuse(List<Card> hand) {
+    return hand.any((card) => card == Card.excuse());
+  }
+
 }
