d61311438c9015fbbadf696854bd81248f0210d5
Olivier Boisard <olivier.boisard@gmail.com>
2020-02-10 12:47:05 +0100

    Replaced var with final in unit test.

--- test/engine/deck_test.dart
+++ test/engine/deck_test.dart
@@ -5,14 +5,14 @@ import 'package:french_tarot/engine/deck.dart';
 
 void main() {
   test("Construct deck without random object", () {
-    var nCardsInDeck = 78;
+    final nCardsInDeck = 78;
     expect(Deck().cards.length, nCardsInDeck);
   });
   test("Construct deck with random object", () {
     expect(() => Deck.withRandom(Random()), returnsNormally);
   });
   test("Shuffle deck", () {
-    var shuffledDeck = Deck();
+    final shuffledDeck = Deck();
     shuffledDeck.shuffle();
     expect(shuffledDeck.cards, isNot(orderedEquals(Deck().cards)));
     expect(shuffledDeck.cards, unorderedEquals(Deck().cards));
