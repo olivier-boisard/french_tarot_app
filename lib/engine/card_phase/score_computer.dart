97847ca85df937f6dbb98bc47d1416e76787786f
Olivier Boisard <olivier.boisard@gmail.com>
2020-03-17 07:31:50 +0100

    Added unit test for excuse handling.

--- lib/engine/card_phase/score_computer.dart
+++ lib/engine/card_phase/score_computer.dart
@@ -1,12 +1,13 @@
-import 'package:french_tarot/engine/card_phase/card_phase_agent.dart';
-import 'package:french_tarot/engine/core/player_state.dart';
-
+import '../core/abstract_card.dart';
+import '../core/card.dart';
+import '../core/player_state.dart';
+import 'card_phase_agent.dart';
 import 'turn.dart';
 
 class ScoreComputer {
-  CardPhaseAgent _taker;
-  PlayerState _takerState;
-  PlayerState _oppositionState;
+  final CardPhaseAgent _taker;
+  final PlayerState _takerState;
+  final PlayerState _oppositionState;
 
   ScoreComputer(this._taker)
       : _takerState = PlayerState(),
@@ -17,12 +18,64 @@ class ScoreComputer {
   int get oppositionScore => _oppositionState.score;
 
   void consume(Turn turn, List<CardPhaseAgent> agentsPlayOrder) {
+    final takerWon = _didTakerWin(agentsPlayOrder, turn);
+    _dealNonExcuseCardsToWinner(turn, takerWon);
+
+    final takerCard = _extractTakerPlayedCard(turn, agentsPlayOrder);
+    _handleExcuse(turn, takerCard, takerWon);
+  }
+
+  Card _extractTakerPlayedCard(Turn turn,
+      List<CardPhaseAgent> agentsPlayOrder) {
+    final takerCard = turn.playedCards[agentsPlayOrder.indexOf(_taker)];
+    return takerCard;
+  }
+
+  bool _didTakerWin(List<CardPhaseAgent> agentsPlayOrder, Turn turn) {
     final winner = agentsPlayOrder[turn.winningCardIndex];
-    final playedCards = turn.playedCards;
-    if (winner == _taker) {
-      _takerState.winCards(playedCards);
+    final takerWon = winner == _taker;
+    return takerWon;
+  }
+
+  void _dealNonExcuseCardsToWinner(Turn turn, bool takerWon) {
+    final playedCardsWithoutExcuse = turn.playedCards.toList()
+      ..remove(const Card.excuse());
+    if (takerWon) {
+      _takerState.winCards(playedCardsWithoutExcuse);
     } else {
-      _oppositionState.winCards(playedCards);
+      _oppositionState.winCards(playedCardsWithoutExcuse);
     }
   }
+
+  void _handleExcuse(Turn turn, Card takerPlayedCard, bool takerWon) {
+    const excuse = Card.excuse();
+    if (turn.playedCards.contains(excuse)) {
+      final takerPlayedExcuse = takerPlayedCard == excuse;
+      if (takerWon && !takerPlayedExcuse) {
+        _oppositionState.winCards([NegativeDummyCard(), excuse]);
+        _takerState.winCards([PositiveDummyCard()]);
+      } else if (!takerWon && takerPlayedExcuse) {
+        _oppositionState.winCards([PositiveDummyCard()]);
+        _takerState.winCards([NegativeDummyCard(), excuse]);
+      }
+    }
+  }
+}
+
+class PositiveDummyCard implements AbstractCard {
+
+  @override
+  double get score => 0.5;
+
+  @override
+  bool get isOudler => false;
+}
+
+class NegativeDummyCard implements AbstractCard {
+
+  @override
+  double get score => -0.5;
+
+  @override
+  bool get isOudler => false;
 }
