286419669063d64187d0e1993d76ab2641204301
Olivier Boisard <olivier.boisard@bd4travel.com>
2020-03-19 07:26:55 +0100

    Added TODO statement.

--- lib/engine/core/turn_consumer.dart
+++ lib/engine/core/turn_consumer.dart
@@ -2,5 +2,6 @@ import 'package:french_tarot/engine/card_phase/abstract_turn.dart';
 
 import '../card_phase/card_phase_agent.dart';
 
+//TODO rename this class
 typedef TurnConsumer<T> = void Function(
     AbstractTurn<T> turn, List<CardPhaseAgent> agentsPlayOrder);
