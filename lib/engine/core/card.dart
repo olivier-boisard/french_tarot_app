8078a970bf97d26245896518a4f8ef94c77859de
Olivier Boisard <olivier.boisard@bd4travel.com>
2020-03-26 06:48:55 +0100

    Broke dependency to flutter.

--- lib/engine/core/card.dart
+++ lib/engine/core/card.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 import 'abstract_card.dart';
 import 'suited_playable.dart';
 
-@immutable
 class Card implements AbstractCard {
   @override
   final Suit suit;
