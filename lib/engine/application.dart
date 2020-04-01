95b2d0b9245a43b46191c51557b21851b76870f1
Olivier Boisard <olivier.boisard@bd4travel.com>
2020-04-01 06:16:10 +0200

    Created dog_dealer file and integrated it into the code.

--- lib/engine/application.dart
+++ lib/engine/application.dart
@@ -1,20 +1,13 @@
 import 'dart:math';
 
-import 'package:french_tarot/engine/dog_dealer.dart';
-
-import 'card_phase/card_phase_agent.dart';
-import 'card_phase/one_use_action_handler.dart';
 import 'card_phase/round.dart';
 import 'card_phase/score_computer.dart';
 import 'card_phase/turn.dart';
+import 'core/abstract_card.dart';
 import 'core/abstract_card_phase_agent.dart';
-import 'core/card.dart';
-import 'core/deck.dart';
 import 'core/function_interfaces.dart';
-import 'core/score_element.dart';
 import 'core/score_manager.dart';
-import 'core/selector.dart';
-import 'core/suited_playable.dart';
+import 'dog_phase/dog_dealer.dart';
 
 class Application {
   final ConfiguredObject _configuredObject;
@@ -22,39 +15,50 @@ class Application {
   Application(this._configuredObject);
 
   void run() {
-    const randomSeed = 1;
-    const nCardsInDog = 6;
-    final takerScoreManager = _configuredObject.takerScoreManager;
-    final oppositionScoreManager = _configuredObject.oppositionScoreManager;
-    final earnedPointsConsumer = _configuredObject.earnedPointsConsumer;
-    final dogDealer = DogDealer(takerScoreManager);
-    final random = Random(randomSeed);
+    final agents = _configuredObject.agents;
 
-    final deck = _createDeck(random);
-    final dog = _createDog(deck, nCardsInDog);
-    final agents = _createAgents(deck, nCardsInDog);
-    final taker = _determineTaker(agents, random);
-    dogDealer.deal(dog);
+    final biddingResult = _runBiddingPhase(agents);
+    _runDogPhase(biddingResult);
+    _runCardPhase(biddingResult, agents);
+  }
 
-    final scoreComputer = ScoreComputer(
-      taker,
-      takerScoreManager,
-      oppositionScoreManager,
-    );
+  void _runCardPhase(
+    BiddingResult biddingResult,
+    List<AbstractCardPhaseAgent> agents,
+  ) {
+    final scoreComputer = _createScoreComputer(biddingResult.taker);
     _playRound(scoreComputer, agents);
-    final earnedPoints = _computeEarnedPoints(takerScoreManager, agents, taker);
-    earnedPointsConsumer(earnedPoints);
+    _distributePoints(agents, biddingResult.taker);
   }
 
-  Deck _createDeck(Random random) {
-    return Deck.withRandom(random)..shuffle();
+  void _runDogPhase(BiddingResult biddingResult) {
+    final takerScoreManager = _configuredObject.takerScoreManager;
+    final dog = _configuredObject.dog;
+    DogDealer(takerScoreManager).deal(biddingResult, dog);
   }
 
-  List<int> _computeEarnedPoints(
-    ScoreManager takerState,
+  void _distributePoints(
     List<AbstractCardPhaseAgent> agents,
     AbstractCardPhaseAgent taker,
   ) {
+    final earnedPoints = _computeEarnedPoints(agents, taker);
+    _configuredObject.earnedPointsConsumer(earnedPoints);
+  }
+
+  ScoreComputer _createScoreComputer(AbstractCardPhaseAgent taker) {
+    final scoreComputer = ScoreComputer(
+      taker,
+      _configuredObject.takerScoreManager,
+      _configuredObject.oppositionScoreManager,
+    );
+    return scoreComputer;
+  }
+
+  List<int> _computeEarnedPoints(
+    List<AbstractCardPhaseAgent> agents,
+    AbstractCardPhaseAgent taker,
+  ) {
+    final takerState = _configuredObject.takerScoreManager;
     final contract = [56, 51, 41, 36][takerState.nOudlers];
     var takerEarnedPoints = 0;
     var opponentsEarnedPoints = 0;
@@ -87,47 +91,38 @@ class Application {
     Round(() => Turn(), scoreComputer.consume).play(agents);
   }
 
-  AbstractCardPhaseAgent _determineTaker(
-      List<AbstractCardPhaseAgent> agents, Random random) {
-    final taker = agents[random.nextInt(agents.length)];
-    return taker;
-  }
-
-  List<Card> _createDog(Deck deck, int nCardsInDog) {
-    return deck.pop(nCardsInDog);
-  }
-
-  List<AbstractCardPhaseAgent> _createAgents(Deck deck, int nCardsInDog) {
-    final nPlayers = _configuredObject.agentDecisionMakers.length;
-    final nCardsToDealToPlayers = deck.originalSize - nCardsInDog;
-    final nCardsPerAgent = nCardsToDealToPlayers ~/ nPlayers;
-    if (nCardsToDealToPlayers % nCardsPerAgent != 0) {
-      throw InvalidAmountOfCardsInDeckException();
-    }
-    final agents = <AbstractCardPhaseAgent>[];
-    for (final agentDecisionMaker in _configuredObject.agentDecisionMakers) {
-      final cardsInHand = _createDog(deck, nCardsPerAgent);
-      final hand = OneUseActionHandler<SuitedPlayable>(cardsInHand);
-      agents.add(CardPhaseAgent(agentDecisionMaker, hand));
-    }
-    return agents;
+  BiddingResult _runBiddingPhase(List<AbstractCardPhaseAgent> agents) {
+    return BiddingResult(
+      agents[_configuredObject.random.nextInt(agents.length)],
+      Bid.PETITE,
+    );
   }
 }
 
 class ConfiguredObject {
-  final List<Selector<SuitedPlayable>> agentDecisionMakers;
+  final List<AbstractCardPhaseAgent> agents;
+  final List<AbstractCard> dog;
   final ScoreManager takerScoreManager;
   final ScoreManager oppositionScoreManager;
   final Consumer<List<int>> earnedPointsConsumer;
-  final Consumer<List<ScoreElement>> dogDealer;
+  final Random random;
 
   ConfiguredObject(
-    this.agentDecisionMakers,
+    this.agents,
+    this.dog,
     this.takerScoreManager,
     this.oppositionScoreManager,
     this.earnedPointsConsumer,
-    this.dogDealer,
+    this.random,
   );
+}
+
+class Bid {
+  static const PASS = 0;
+  static const PETITE = 1;
+  static const GUARD = 2;
+  static const GUARD_WITHOUT = 3;
+  static const GUARD_AGAINST = 4;
 }
 
 class InvalidAmountOfCardsInDeckException implements Exception {}
--- test/engine/application_test.dart
+++ test/engine/application_test.dart
@@ -2,13 +2,14 @@ import 'dart:math';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:french_tarot/engine/application.dart';
-import 'package:french_tarot/engine/core/score_element.dart';
+import 'package:french_tarot/engine/card_phase/card_phase_agent.dart';
+import 'package:french_tarot/engine/card_phase/one_use_action_handler.dart';
+import 'package:french_tarot/engine/core/abstract_card_phase_agent.dart';
+import 'package:french_tarot/engine/core/deck.dart';
 import 'package:french_tarot/engine/core/score_manager.dart';
 import 'package:french_tarot/engine/core/selector.dart';
 import 'package:french_tarot/engine/core/suited_playable.dart';
 import 'package:french_tarot/engine/random_decision_maker.dart';
-
-void dummyDogConsumer(List<ScoreElement> dog) {}
 
 void main() {
   test('Run application', () {
@@ -31,25 +32,44 @@ void main() {
 List<int> runApplication() {
   const randomSeed = 1;
   const nPlayers = 4;
+  const nCardsInDog = 6;
 
   final random = Random(randomSeed);
-  final decisionMaker =
-      RandomDecisionMaker<SuitedPlayable>.withRandom(random).run;
-  final agentDecisionMakers = <Selector<SuitedPlayable>>[];
+  final decisionMaker = RandomDecisionMaker<SuitedPlayable>.withRandom(
+    random,
+  ).run;
+  final decisionMakers = <Selector<SuitedPlayable>>[];
   for (var i = 0; i < nPlayers; i++) {
-    agentDecisionMakers.add(decisionMaker);
+    decisionMakers.add(decisionMaker);
   }
 
   final earnedPoints = <int>[];
   final takerScoreManager = ScoreManager();
   final oppositionScoreManager = ScoreManager();
+  final deck = Deck.withRandom(random)..shuffle();
+  final dog = deck.pop(nCardsInDog);
   final configuredObject = ConfiguredObject(
-    agentDecisionMakers,
-    takerScoreManager,
-    oppositionScoreManager,
-    earnedPoints.addAll,
-    dummyDogConsumer,
-  );
+      _createAgents(deck, decisionMakers, deck.originalSize - dog.length),
+      dog,
+      takerScoreManager,
+      oppositionScoreManager,
+      earnedPoints.addAll,
+      random);
   Application(configuredObject).run();
   return earnedPoints;
+}
+
+List<AbstractCardPhaseAgent> _createAgents(Deck deck,
+    List<Selector<SuitedPlayable>> decisionMakers, int nCardsToDeal) {
+  final nCardsPerAgent = nCardsToDeal ~/ decisionMakers.length;
+  if (nCardsToDeal % nCardsPerAgent != 0) {
+    throw InvalidAmountOfCardsInDeckException();
+  }
+  final agents = <AbstractCardPhaseAgent>[];
+  for (final decisionMaker in decisionMakers) {
+    final cardsInHand = deck.pop(nCardsPerAgent);
+    final hand = OneUseActionHandler<SuitedPlayable>(cardsInHand);
+    agents.add(CardPhaseAgent(decisionMaker, hand));
+  }
+  return agents;
 }
