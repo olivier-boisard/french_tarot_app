9602b075289044a6b450d2195263f7a86fe1a12a
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-06 07:22:18 +0200

    Removed useless _Hand class.

--- lib/engine/phases/card/card_phase_agent.dart
+++ lib/engine/phases/card/card_phase_agent.dart
@@ -7,36 +7,23 @@ import '../../core/suited_playable.dart';
 
 //TODO break dependency to OneUseActionHandler
 class CardPhaseAgent implements AbstractCardPhaseAgent {
-  final _Hand _hand;
   final Selector<SuitedPlayable> _decisionMaker;
+  final List<SuitedPlayable> _hand;
 
-  CardPhaseAgent(this._decisionMaker, List<SuitedPlayable> handCards)
-      : _hand = _Hand(handCards);
+  CardPhaseAgent(this._decisionMaker, this._hand);
 
   @override
   Decision<SuitedPlayable> play(State<SuitedPlayable> turn) {
-    return _hand.pickAction(wrapSelector(turn, _decisionMaker));
-  }
-
-  @override
-  bool get isReady => !_hand.isEmpty;
-}
-
-class EmptyHandException implements Exception {}
-
-class _Hand {
-  final List<SuitedPlayable> _actions;
-
-  _Hand(this._actions);
-
-  Decision<SuitedPlayable> pickAction(Selector<SuitedPlayable> decisionMaker) {
-    if (_actions.isEmpty) {
+    if (_hand.isEmpty) {
       throw EmptyHandException();
     }
-    final decision = decisionMaker(_actions);
-    _actions.remove(decision.action);
+    final decision = wrapSelector(turn, _decisionMaker)(_hand);
+    _hand.remove(decision.action);
     return decision;
   }
 
-  bool get isEmpty => _actions.isEmpty;
+  @override
+  bool get isReady => _hand.isNotEmpty;
 }
+
+class EmptyHandException implements Exception {}
