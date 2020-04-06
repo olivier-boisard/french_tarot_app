4a0d53dd106ae0ce8eeca6e35f5651b904f785e1
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-06 07:28:00 +0200

    Merged _wrapSelector in CardPhaseAgent.

--- lib/engine/core/selector_wrapper.dart
+++
@@ -1,6 +0,0 @@
-import 'environment_state.dart';
-import 'selector.dart';
-
-Selector<T> wrapSelector<T>(State<T> state, Selector<T> selector) {
-  return (actions) => selector(state.extractAllowedActions(actions));
-}
--- lib/engine/phases/card/card_phase_agent.dart
+++ lib/engine/phases/card/card_phase_agent.dart
@@ -2,7 +2,6 @@ import '../../core/abstract_card_phase_agent.dart';
 import '../../core/decision.dart';
 import '../../core/environment_state.dart';
 import '../../core/selector.dart';
-import '../../core/selector_wrapper.dart';
 import '../../core/suited_playable.dart';
 
 class CardPhaseAgent implements AbstractCardPhaseAgent {
@@ -16,13 +15,18 @@ class CardPhaseAgent implements AbstractCardPhaseAgent {
     if (_hand.isEmpty) {
       throw EmptyHandException();
     }
-    final decision = wrapSelector(turn, _decisionMaker)(_hand);
+    final decision = _wrapSelector(turn, _decisionMaker)(_hand);
     _hand.remove(decision.action);
     return decision;
   }
 
   @override
   bool get isReady => _hand.isNotEmpty;
+
+  static Selector<T> _wrapSelector<T>(State<T> state, Selector<T> selector) {
+    return (actions) => selector(state.extractAllowedActions(actions));
+  }
+
 }
 
 class EmptyHandException implements Exception {}
