84ce55c131f7181e0693dcf0418a1a1007a2fdf3
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-07 07:39:40 +0200

    Added 'Winner plays first next round' unit test.

--- test/engine/round_test.dart
+++ test/engine/round_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:french_tarot/engine/core/card.dart';
+import 'package:french_tarot/engine/core/decision.dart';
 import 'package:french_tarot/engine/core/round_scores_computer.dart';
 import 'package:french_tarot/engine/core/score_manager.dart';
+import 'package:french_tarot/engine/core/suited_playable.dart';
 import 'package:french_tarot/engine/core/tarot_deck_facade.dart';
 import 'package:french_tarot/engine/phases/card/card_phase.dart';
 import 'package:french_tarot/engine/phases/card/card_phase_agent.dart';
@@ -38,7 +41,36 @@ void main() {
   });
 
   //TODO add test to make sure winner of one round gets to play first after
-  test('Winner plays first next round', (){
+  test('Winner plays first next round', () {
+    final agents = [
+      CardPhaseAgent(pickFirst, [
+        Card.coloredCard(Suit.spades, 1),
+        Card.coloredCard(Suit.spades, 2),
+      ]),
+      CardPhaseAgent(pickFirst, [
+        Card.coloredCard(Suit.spades, 3),
+        Card.coloredCard(Suit.heart, 1),
+      ])
+    ];
 
+    final takerScoreManager = ScoreManager();
+    final oppositionScoreManager = ScoreManager();
+
+    //TODO not very satisfying to create an object that is not ready
+    final roundScoresComputer = RoundScoresComputer(
+      takerScoreManager.winScoreElements,
+      oppositionScoreManager.winScoreElements,
+    );
+    roundScoresComputer.taker = agents[0];
+
+    CardPhase(() => Turn(), roundScoresComputer.consume, agents).run();
+
+    expect(takerScoreManager, equals(0));
+    expect(oppositionScoreManager, equals(1));
   });
+}
+
+Decision<SuitedPlayable> pickFirst(List<SuitedPlayable> allowedActions) {
+  const probability = 1.0;
+  return Decision(probability, allowedActions.first);
 }
