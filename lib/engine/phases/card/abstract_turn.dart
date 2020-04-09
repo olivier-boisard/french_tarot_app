f7751cabbf584ac1f9cd1a4e3d8c7b106effaf7d
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-09 07:31:06 +0200

    Fixed linter false positive.

--- lib/engine/phases/card/abstract_turn.dart
+++ lib/engine/phases/card/abstract_turn.dart
@@ -1,6 +1,8 @@
 import '../../core/actions_handler.dart';
 import '../../core/environment_state.dart';
 
-abstract class AbstractTurn<T> implements State<T>, ActionsHandler<T> {
+// This class exists instead of a simple functional interface because it also
+// implements other interfaces
+abstract class AbstractTurn<T> implements State<T>, ActionsHandler<T> { // ignore: one_member_abstracts
   void addAction(T action);
 }
