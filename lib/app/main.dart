620b90e456fa7b01c7a7eeba71fa11e41aa5670f
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-20 07:09:44 +0200

    Removed main() function.

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../engine/core/card.dart' as engine;
 import 'game_page.dart';
 
 // Got inspirations from https://medium.com/flutter-community/creating-solitaire-in-flutter-946c34ef053c
 
-void main() => runApp(FrenchTarotApp());
-
 class FrenchTarotApp extends StatelessWidget {
+  final List<engine.Card> _visibleHand;
+
+  FrenchTarotApp(this._visibleHand, {Key key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: GamePage(),
+      title: 'French Tarot',
+      home: GamePage(_visibleHand),
     );
   }
 }
