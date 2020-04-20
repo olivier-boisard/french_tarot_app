38623fba226026f28f71c4d74811f9f44c4f102c
Olivier Boisard <olivier.boisard@gmail.com>
2020-04-20 07:06:59 +0200

    Removed unnecessary const statements.

--- lib/app/game_page.dart
+++ lib/app/game_page.dart
@@ -20,13 +20,14 @@ class _GamePageState extends State<GamePage> {
 
   @override
   Widget build(BuildContext context) {
-    const faceDownArea = FaceDownArea(_visibleHand.length);
+    final faceDownArea = FaceDownArea(_visibleHand.length);
+
     return Scaffold(
       backgroundColor: Colors.green[800],
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          const Expanded(
+          Expanded(
             flex: 1,
             child: faceDownArea,
           ),
@@ -39,7 +40,7 @@ class _GamePageState extends State<GamePage> {
                   flex: 1,
                   child: Align(
                     alignment: Alignment.centerLeft,
-                    child: const RotatedBox(
+                    child: RotatedBox(
                       quarterTurns: 1,
                       child: faceDownArea,
                     ),
@@ -53,7 +54,7 @@ class _GamePageState extends State<GamePage> {
                   flex: 1,
                   child: Align(
                     alignment: Alignment.centerRight,
-                    child: const RotatedBox(
+                    child: RotatedBox(
                       quarterTurns: 3,
                       child: faceDownArea,
                     ),
@@ -67,7 +68,6 @@ class _GamePageState extends State<GamePage> {
             flex: 1,
             child: Align(
               alignment: Alignment.bottomCenter,
-              //TODO this won't compile
               child: FaceUpArea(_visibleHand),
             ),
           ),
