687db590da901a37d65e9a43556b64542edf3c03
Olivier Boisard <olivier.boisard@gmail.com>
2020-05-14 07:32:32 +0200

    Test cards played by other player also show up.

--- test/app/face_up_card_test.dart
+++ test/app/face_up_card_test.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:french_tarot/app/cards/face_down_card.dart';
@@ -14,21 +15,26 @@ import 'package:french_tarot/engine/core/suited_playable.dart';
 import 'package:french_tarot/engine/core/tarot_card.dart';
 
 void main() {
-  const playedCardWidgetText = '1♠';
+  const cardToPlayKey = Key('cardToPlay');
+  final card = TarotCard.coloredCard(Suit.spades, 1);
   testWidgets('Hand cards are visible', (tester) async {
-    await _prepareApp(tester);
+    await _prepareApp(tester, card, cardToPlayKey);
 
-    expect(find.text('1\n♠'), findsWidgets);
-    expect(find.text(playedCardWidgetText), findsOneWidget);
+    expect(find.byKey(cardToPlayKey), findsOneWidget);
   });
 
   testWidgets('Play card', (tester) async {
-    const cardToPlayKey = Key('cardToPlay');
     const cardToPlayTargetKey = Key('cardToPlayTarget');
 
     await _prepareApp(
       tester,
-      cardToPlayKey: cardToPlayKey,
+      card,
+      cardToPlayKey,
+      cardPlayedByOthers: <AbstractTarotCard>[
+        TarotCard.coloredCard(Suit.spades, 2),
+        TarotCard.coloredCard(Suit.spades, 3),
+        TarotCard.coloredCard(Suit.spades, 4),
+      ],
       cardToPlayTargetKey: cardToPlayTargetKey,
     );
 
@@ -40,23 +46,42 @@ void main() {
 
     final playedCardFinder = find.descendant(
       of: find.byType(PlayedCardsArea),
-      matching: find.widgetWithText(FaceUpCard, playedCardWidgetText),
+      matching: find.widgetWithText(FaceUpCard, '1♠'),
+    );
+
+    final secondOfSpades = find.descendant(
+      of: find.byType(PlayedCardsArea),
+      matching: find.widgetWithText(FaceUpCard, '2♠'),
+    );
+    final thirdOfSpades = find.descendant(
+      of: find.byType(PlayedCardsArea),
+      matching: find.widgetWithText(FaceUpCard, '3♠'),
+    );
+    final fourthOfSpades = find.descendant(
+      of: find.byType(PlayedCardsArea),
+      matching: find.widgetWithText(FaceUpCard, '4♠'),
     );
 
     expect(playedCardFinder, findsOneWidget);
+    expect(secondOfSpades, findsOneWidget);
+    expect(thirdOfSpades, findsOneWidget);
+    expect(fourthOfSpades, findsOneWidget);
+
+    //TODO test played card not in hand anymore
   });
 }
 
-Future _prepareApp(WidgetTester tester,
-    {Key cardToPlayKey, Key cardToPlayTargetKey}) async {
-  final card = TarotCard.coloredCard(Suit.spades, 1);
+//TODO refactor
+Future _prepareApp(WidgetTester tester, AbstractTarotCard cardPlayedByUser,
+    Key cardToPlayKey, {List<AbstractTarotCard> cardPlayedByOthers,
+      Key cardToPlayTargetKey}) async {
   final dimensions = Dimensions.fromScreen();
   final visibleCards = <Draggable<AbstractTarotCard>>[
     Draggable<AbstractTarotCard>(
       key: cardToPlayKey,
-      data: card,
-      feedback: FaceUpCard(card: card, dimensions: dimensions),
-      child: FaceUpCard(card: card, dimensions: dimensions),
+      data: cardPlayedByUser,
+      feedback: FaceUpCard(card: cardPlayedByUser, dimensions: dimensions),
+      child: FaceUpCard(card: cardPlayedByUser, dimensions: dimensions),
     ),
   ];
 
@@ -78,9 +103,25 @@ Future _prepareApp(WidgetTester tester,
       playedCards: playedCards,
       cardIsAllowed: (card) => true,
       playCard: (card) {
+        // Display card that was played by user
+        final cardDimensions = Dimensions.fromScreen();
         playedCards[PlayerLocation.bottom] = FaceUpCard(
           card: card,
-          dimensions: Dimensions.fromScreen(),
+          dimensions: cardDimensions,
+        );
+
+        // Display other cards
+        playedCards[PlayerLocation.left] = FaceUpCard(
+          card: cardPlayedByOthers[0],
+          dimensions: cardDimensions,
+        );
+        playedCards[PlayerLocation.top] = FaceUpCard(
+          card: cardPlayedByOthers[1],
+          dimensions: cardDimensions,
+        );
+        playedCards[PlayerLocation.right] = FaceUpCard(
+          card: cardPlayedByOthers[2],
+          dimensions: cardDimensions,
         );
       },
       cardDraggableTargetKey: cardToPlayTargetKey,
