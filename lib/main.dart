0abb9f16c0fc2556d487d10f3de3fa0d0edab448
Olivier Boisard <olivier.boisard@gmail.com>
2020-06-24 08:32:52 +0200

    Emptied main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'engine/core/tarot_deck_facade.dart';
-
-//TODO replace List with Iterable everywhere possible in the code
 
 void main() {
-  final deck = TarotDeckFacade()..shuffle();
-  const nCards = 18;
 
-  final cards = deck.pop(nCards);
 }
